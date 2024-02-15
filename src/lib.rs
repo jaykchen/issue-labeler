01a737ece88bd04581d5905e6d99aa46b21d0727
Ji Chen <jaykchen@gmail.com>
2024-02-15 18:32:44 -0500

    hourly

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ pub async fn on_deploy() {
     // let now = Utc::now();
     // let now_minute = now.minute() + 2;
     // let cron_time = format!("{:02} {:02} {:02} * *", now_minute, now.hour(), now.day());
-    let cron_time = format!("2 2 * * *");
+    let cron_time = format!("2 * * * *");
     schedule_cron_job(cron_time, String::from("cron_job_evoked")).await;
 }
 
